f20e6614de3d8dcfcde449a8bdade3f6d298b674
Luca Seri <lucaseri2003@gmail.com>
2023-05-22 19:49:05 -0300

    Cambios pequeños para probar

--- src/whist/impresion_curses.py
+++ src/whist/impresion_curses.py
@@ -7,8 +7,8 @@ def print_menu(ventana_v, selected_row_idx, seleccion):
         x = w//2 - len(row)//2
         y = h//2 - len(seleccion)//2 + idx
         if idx == selected_row_idx:
-            ventana_v.attron(curses.color_pair(1))
             ventana_v.addstr(y, x, row)
+            ventana_v.attron(curses.color_pair(1))
             ventana_v.attroff(curses.color_pair(2))
         else:
           ventana_v.addstr(y, x, row)
@@ -23,14 +23,16 @@ def imprimir_seleccion_modo(stdscr, altura_p: int, largo_p: int) -> None:
     coordx_nombre = largo_p//2 - len(nombre_juego)//2
     coordy_nombre = altura_p//5
     coordx_seleccion = largo_p//2 - tamaño_seleccion[1]//2
-    coordy_seleccion = altura_p//2
+    coordy_seleccion = altura_p//2 - tamaño_seleccion[0]//2
     condicion_seleccion = True
 
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_CYAN)
     nombre_v = curses.newwin(tamaño_nombre[0], tamaño_nombre[1], coordy_nombre, coordx_nombre)
-    nombre_v.bkgd(' ', curses.color_pair(2))
+    # nombre_v.bkgd(' ', curses.color_pair(2))
+    nombre_v.bkgdset(curses.color_pair(2))
     nombre_v.border()
     nombre_v.addstr(1, 2, nombre_juego, curses.A_UNDERLINE | curses.A_BOLD)
+    nombre_v.refresh()
 
     seleccion_v = curses.newwin(tamaño_seleccion[0], tamaño_seleccion[1], coordy_seleccion, coordx_seleccion)
     seleccion_v.border()
@@ -38,17 +40,17 @@ def imprimir_seleccion_modo(stdscr, altura_p: int, largo_p: int) -> None:
     current_row = 0
     while condicion_seleccion:
         key = seleccion_v.getch()
-
+        seleccion_v.addstr(f"{key == 'a'}")
         if key == curses.KEY_UP and current_row > 0:
             current_row -= 1
-        elif key == curses.KEY_DOWN and current_row < len(seleccion)-1:
+        # elif key == curses.KEY_DOWN and current_row < len(seleccion)-1:
+        elif key == curses.KEY_DOWN:
             current_row += 1
-        elif key == curses.KEY_ENTER or key in [10, 13]:
-            seleccion_v.getch()
-    		# if user selected last row, exit the program
-            if current_row == len(seleccion)-1:
-                break
-        
+        # elif key == curses.KEY_ENTER or key in [10, 13]:
+        #     seleccion_v.getch()
+    	# 	# if user selected last row, exit the program
+        #     if current_row == len(seleccion)-1:
+        #         break
         print_menu(seleccion_v, current_row, seleccion)
 
     # nombre_v.getch()
@@ -63,8 +65,10 @@ def imprimir_seleccion_modo(stdscr, altura_p: int, largo_p: int) -> None:
 def main(stdscr):
 
     curses.curs_set(False)
-    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
     stdscr.bkgd(' ', curses.color_pair(1)) # Ojo, este método no se refresca automáticamente
+    stdscr.refresh()
+    stdscr.getch()
     altura_p, largo_p = stdscr.getmaxyx()
     imprimir_seleccion_modo(stdscr, altura_p, largo_p)
     
